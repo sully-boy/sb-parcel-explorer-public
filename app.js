62dde9b3df9f918f3265070c4603c721b121c656
sully-boy <sully@sb-designgroup.com>
2026-08-26 16:47:12 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -71,7 +71,8 @@ const BASEMAPS = {
   }),
   light: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
     attribution: '© Esri, HERE, Garmin, © OpenStreetMap contributors',
-    maxZoom: 16
+    maxZoom: 19,
+    maxNativeZoom: 16
   }),
 };
 
